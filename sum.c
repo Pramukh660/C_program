341977facb69694fe9dd53990e393e712fa29653
pramukhjs <pramukh660@gmail.com>
2021-04-21 11:39:37 +0530

    function with loop

---
+++ sum.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int recursion(int n) //recursive function till n=1
+{
+    if(n==1)
+        return 1;
+    else
+        return n + recursion(n-1);
+}
+
+int direct(int n)
+{
+    return n*(n+1)/2; //using formula of summation of n
+}
+
+int loop(int n)
+{
+    int sum=0;
+    for (int i = 1; i <= n; ++i)
+        sum += i;
+    return sum;
+}
+
+void main()
+{
+    int n;
+    printf("Enter the nth term\n");
+    scanf("%d",&n);
+    printf("Sum of first %d natural no is %d\n",n,recursion(n));
+    printf("Sum of first %d natural no is %d\n",n,direct(n));
+    printf("Sum of first %d natural no is %d\n",n,loop(n));
+}
